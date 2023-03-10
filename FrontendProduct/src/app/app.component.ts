d2fb79c18b4bb01696be37a82ac8aaeb4832f90b
Carlos Duarte <carlosduarte.1@hotmail.com>
2023-03-09 21:18:42 -0600

    fix: styles responsive added

--- FrontendProduct/src/app/app.component.ts
+++ FrontendProduct/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Meta } from '@angular/platform-browser';  
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FrontendProduct';
+
+  constructor(private meta: Meta) {
+    this.meta.addTags([
+      { name: 'viewport', content: 'width=device-width, initial-scale=1.0' }  
+    ]); 
+  } 
 }
